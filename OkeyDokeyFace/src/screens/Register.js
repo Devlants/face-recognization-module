b7104030df1bed68f8f8b75dabb94e4b55566b80
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-04-11 03:03:52 +0900

    add CameraScreen

--- OkeyDokeyFace/src/screens/Register.js
+++ OkeyDokeyFace/src/screens/Register.js
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import CameraScreen from '../components/CameraScreen';
 
 const Register = () => {
   return (
-    <View>
-      <Text>내 화면</Text>
+    <View style={{flex: 1}}>
+      <Text>일단 카메라스크린 넣기</Text>
+      <CameraScreen />
     </View>
   );
 };
